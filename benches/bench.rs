e0c9dbf092052c25ce35b98467233b5d99de50d1
qdot3 <quantum.engine.55g@gmail.com>
2026-08-25 14:06:21 +0900

    modified

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,7 +1,6 @@
 use std::{
     cell::{Cell, RefCell},
     hint::black_box,
-    time::Duration,
 };
 
 use cache_size::{l1_cache_line_size, l1_cache_size};
@@ -30,35 +29,45 @@ fn cache_info() -> String {
     format!("L1: {l1_size}, L1 Line: {l1_line_size}, word: {word_size}")
 }
 
-fn add_1(c: &mut Criterion) {
+fn add1_cell<const N: usize>(src: &Cell<[usize; N]>) {
+    let mut val = src.get();
+    val.iter_mut().for_each(|v| {
+        *v = v.wrapping_add(
+            // avoid auto-vectorization
+            black_box(1),
+        )
+    });
+    src.set(val);
+}
+
+fn add1_refcell<const N: usize>(src: &RefCell<[usize; N]>) {
+    let mut val = src.borrow_mut();
+    val.iter_mut()
+        .for_each(|v| *v = v.wrapping_add(black_box(1)));
+}
+
+fn add1(c: &mut Criterion) {
     let mut b = c.benchmark_group(format!("Add 1 (words vs time) ({})", cache_info()));
-    b.sample_size(500).measurement_time(Duration::from_secs(10));
 
     macro_rules! bench {
         ($( $e:expr ),*) => {$(
-            let src = black_box([0usize; $e]);
+            let src = [0; $e];
 
             let cell = Cell::new(src);
             b.bench_with_input(BenchmarkId::new("Cell", $e), &cell, |b, i| {
-                b.iter(|| {
-                    let mut v = i.get();
-                    v.iter_mut().for_each(|v| *v = v.wrapping_add(black_box(1)));
-                    i.set(v);
-                })
+                b.iter(|| add1_cell(black_box(i)))
             });
 
-            let ref_cell = RefCell::new(src);
-            b.bench_with_input(BenchmarkId::new("RefCell", $e), &ref_cell, |b, i| {
-                b.iter(|| {
-                    i.borrow_mut().iter_mut().for_each(|v| *v = v.wrapping_add(black_box(1)));
-                })
+            let refcell = RefCell::new(src);
+            b.bench_with_input(BenchmarkId::new("RefCell", $e), &refcell, |b, i| {
+                b.iter(|| add1_refcell(black_box(i)))
             });
         )*};
     }
     bench!(
-        2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48
+        2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40
     );
 }
 
-criterion_group!(benches, add_1);
+criterion_group!(benches, add1);
 criterion_main!(benches);
