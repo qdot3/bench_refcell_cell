e905f2a83101f7a46d7475b6fe2c38895d5a00be
qdot3 <quantum.engine.55g@gmail.com>
2026-08-25 10:03:33 +0900

    refine benchmark

--- benches/bench.rs
+++ benches/bench.rs
@@ -6,14 +6,16 @@ use std::{
 
 use criterion::{BenchmarkId, Criterion, criterion_group, criterion_main};
 
-fn reverse(c: &mut Criterion) {
-    let mut b = c.benchmark_group("Reverse");
+fn add_1(c: &mut Criterion) {
+    let mut b = c.benchmark_group("Add 1 (words vs time)");
     b.sample_size(500).measurement_time(Duration::from_secs(10));
 
     macro_rules! bench {
         ($( $e:expr ),*) => {$(
-            let cell = Cell::new([0u8; $e]);
-            b.bench_with_input(BenchmarkId::new("Cell", $e), &black_box(cell), |b, i| {
+            let src = black_box([0usize; $e]);
+
+            let cell = Cell::new(src);
+            b.bench_with_input(BenchmarkId::new("Cell", $e), &cell, |b, i| {
                 b.iter(|| {
                     let mut v = i.get();
                     v.iter_mut().for_each(|v| *v = v.wrapping_add(black_box(1)));
@@ -21,16 +23,18 @@ fn reverse(c: &mut Criterion) {
                 })
             });
 
-            let ref_cell = RefCell::new([0u8; $e]);
-            b.bench_with_input(BenchmarkId::new("RefCell", $e), &black_box(ref_cell), |b, i| {
+            let ref_cell = RefCell::new(src);
+            b.bench_with_input(BenchmarkId::new("RefCell", $e), &ref_cell, |b, i| {
                 b.iter(|| {
                     i.borrow_mut().iter_mut().for_each(|v| *v = v.wrapping_add(black_box(1)));
                 })
             });
         )*};
     }
-    bench!(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
+    bench!(
+        2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48
+    );
 }
 
-criterion_group!(benches, reverse);
+criterion_group!(benches, add_1);
 criterion_main!(benches);
